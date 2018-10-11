083f2d01fc4f32dc30c174ccf515e94fe07f0eae
Maïkoff Serge-Jurij <mai714@manor.ch>
2018-10-11 10:27:44 +0200

    added handles, trying to set up some locations in abstract figure

--- src/main/java/jdraw/figures/AbstractFigure.java
+++ src/main/java/jdraw/figures/AbstractFigure.java
@@ -4,6 +4,10 @@ import jdraw.framework.Figure;
 import jdraw.framework.FigureEvent;
 import jdraw.framework.FigureHandle;
 import jdraw.framework.FigureListener;
+import jdraw.handles.*;
+
+import java.awt.*;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -12,11 +16,20 @@ public abstract class AbstractFigure implements Figure {
 
 
     private CopyOnWriteArrayList<FigureListener> myFigureListener = new CopyOnWriteArrayList<>();
-
+    protected Point north, south, east, west, southWest, southEast, northWest, northEast;
 
     @Override
     public List<FigureHandle> getHandles() {
-        return null;
+        List<FigureHandle> handles = new LinkedList<>();
+        handles.add(new NorthHandle(this));
+        handles.add(new SouthHandle(this));
+        handles.add(new WestHandle(this));
+        handles.add(new EastHandle(this));
+        handles.add(new NorthWestHandle(this));
+        handles.add(new NorthEastHandle(this));
+        handles.add(new SouthEastHandle(this));
+        handles.add(new SouthWestHandle(this));
+        return handles;
     }
 
     @Override
@@ -41,4 +54,20 @@ public abstract class AbstractFigure implements Figure {
             listener.figureChanged(figureEvent);
         }
     }
+
+    public void setAllPoints(){
+        north.setLocation(getBounds().x + getBounds().width / 2, getBounds().y);
+        northWest.setLocation(getBounds().getLocation());
+        northEast.setLocation(getBounds().x + getBounds().width, getBounds().y);
+
+        south.setLocation(getBounds().x + getBounds().height, getBounds().y + getBounds().width / 2 );
+        southWest.setLocation(getBounds().x + getBounds().height, getBounds().y);
+        southEast.setLocation(getBounds().x + getBounds().height, getBounds().y + getBounds().width);
+
+        west.setLocation(getBounds().x + getBounds().height / 2, getBounds().y);
+
+        east.setLocation(getBounds().x + getBounds().height / 2, getBounds().y + getBounds().width);
+    }
+
+    
 }
--- src/main/java/jdraw/handles/SouthHandle.java
+++ src/main/java/jdraw/handles/SouthHandle.java
@@ -1,9 +1,38 @@
 package jdraw.handles;
 
+import jdraw.framework.DrawView;
+import jdraw.framework.Figure;
 import jdraw.framework.FigureHandle;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by Geryones on 09/10/2018.
  */
 public class SouthHandle extends AbstractHandles implements FigureHandle {
+
+    public SouthHandle(Figure owner){
+        super(owner);
+    }
+
+    @Override
+    public Point getLocation() {
+        return null;
+    }
+
+    @Override
+    public void startInteraction(int x, int y, MouseEvent e, DrawView v) {
+
+    }
+
+    @Override
+    public void dragInteraction(int x, int y, MouseEvent e, DrawView v) {
+
+    }
+
+    @Override
+    public void stopInteraction(int x, int y, MouseEvent e, DrawView v) {
+
+    }
 }
